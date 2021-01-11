d4beff6151a21c47e43bfa4ec6c4584769c4a9e1
tobi <1356094679@qq.com>
2021-01-11 10:51:20 +0800

    Initial commit

---
+++ xunyee-api/src/main/java/com/vlinkage/api/XunyeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.vlinkage.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunyeeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XunyeeApiApplication.class, args);
+    }
+
+}
---
+++ xunyee-api/src/test/java/com/vlinkage/api/XunyeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlinkage.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XunyeeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
